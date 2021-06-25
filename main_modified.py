ab2d29668b5e7fe1e6f161a69276fbbd520e01af
Naheed <naheedcse@gmail.com>
2021-06-25 15:46:52 +0800

    Added utility module containing wrappers of various hypernetx functions. Major changes to previous code

--- main_modified.py
+++ main_modified.py
@@ -1,11 +1,9 @@
 import sys
-sys.path.append("HyperNetX")
-import matplotlib.pyplot as plt
-import networkx as nx
-import hypernetx as hnx
-import warnings
-import json
 
+sys.path.append("HyperNetX")
+import hypernetx as hnx
+
+import utils
 
 scenes = {
     0: ('FN', 'TH'),
@@ -18,7 +16,6 @@ scenes = {
     7: ('MA', 'GP')
 }
 
-
 H = hnx.Hypergraph(scenes)
 
 # # Visualise hypergraph for verification purposes
@@ -28,24 +25,23 @@ H = hnx.Hypergraph(scenes)
 
 nodes = list(H.nodes)
 num_nodes = len(nodes)
-core = {} 
-bucket = {} # mapping of number of neighbors, say n, to nodes with exactly n neighbors 
-
+core = {}
+bucket = {}  # mapping of number of neighbors, say n, to nodes with exactly n neighbors
 
 # auxiliary data, that improves efficiency
 # node_to_neighbors = {} # Not necessary, I guess
-node_to_num_neighbors = {} # inverse bucket
+node_to_num_neighbors = {}  # inverse bucket
 # removed_nodes = []
 
 
 # Initial bucket fill-up
 for node in nodes:
     neighbors = list(H.neighbors(node))
-    len_neighbors = len(neighbors) # this computation can be repeated
+    len_neighbors = len(neighbors)  # this computation can be repeated
     # node_to_neighbors[node] = neighbors
     node_to_num_neighbors[node] = len_neighbors
     # print(node, neighbors)
-    if(len_neighbors not in bucket):
+    if len_neighbors not in bucket:
         bucket[len_neighbors] = [node]
     else:
         bucket[len_neighbors].append(node)
@@ -55,76 +51,64 @@ for node in H.nodes:
     print(node, H.neighbors(node))
 print()
 
-
 print("\n---------- Initial bucket -------")
 print(bucket)
 print()
 
 for k in range(1, num_nodes + 1):
-    
+
     # TODO Discuss with Naheed vai about this case
     # if(k not in bucket):
     #     continue
 
     # Inner while loop
     # assert k in bucket
-    while len(bucket.get(k,[])) != 0:
-        v = bucket[k].pop(0) # get first element in the
+    while len(bucket.get(k, [])) != 0:
+        v = bucket[k].pop(0)  # get first element in the
         print("k:", k, "node:", v)
         core[v] = k
-        temp_nodes = list(H.nodes)
-        temp_nodes.remove(v) # V' <- V \ {v}
-        # removed_nodes.append(v)
-        # print(nodes)
+        temp_nodes = nodes[:]  # (Naheed:) Make a copy of V. list(H.nodes) is not the same as V, because list(H\{
+        # v}.nodes) is not the same as V\{v}
+        temp_nodes.remove(v)  # V' <- V \ {v}
 
-        H_temp = H.restrict_to_nodes(temp_nodes)
+        # H_temp = H.restrict_to_nodes(temp_nodes) # (Naheed:) This is not what we want.
+        H_temp = utils.strong_subgraph(H, temp_nodes)
 
         # enumerating over all neighbors of v
-        for u in H.neighbors(v):
+        # for u in H.neighbors(v):
         # for u in node_to_neighbors[v]:
-
-            """
-                In each iteration, v is removed. Therefore, node_to_neighbors has to be updated too. 
-                Alternate implementation is to only consider remaining neighbors of v that are not yet removed.
-            """
-            # if(u in removed_nodes):
-            #     continue
-
+        for u in utils.get_nbrs(H, v):  # (Naheed:) H.neighbors(v) does not return [] when v \notin H. Wrote a wrapper.
 
             print(node_to_num_neighbors)
 
             print("Considering neighbor", u)
-            neighbors_u = list(H_temp.neighbors(u))
-            len_neighbors_u = len(neighbors_u) # repeated computaion, hence a variable is declared
-            print(u, 'has neighbors on sub-hypergraph:', neighbors_u)
+            # neighbors_u = list(H_temp.neighbors(u))  # (Naheed: ) Unnecessary
+            # len_neighbors_u = len(neighbors_u)  # (Naheed:) Buggy. Hypernetx neighbors() function may return None
+            # print(u, 'has neighbors on sub-hypergraph:', neighbors_u)
+
+            len_neighbors_u = utils.get_number_of_nbrs(H_temp, u)  # To avoid bug, we use our wrapper function.
 
             max_value = max(len_neighbors_u, k)
-            print("max core between", k, 'and',len_neighbors_u, "is ", max_value)
+            print("max core between", k, 'and', len_neighbors_u, "is ", max_value)
             print("The location of", u, "is updated from", node_to_num_neighbors[u], "to", max_value)
             bucket[node_to_num_neighbors[u]].remove(u)
-            
+
             # Move u to new location in bucket
-            if(max_value not in bucket):
-                #TODO does code reach here?
+            if max_value not in bucket:
+                # TODO does code reach here?
                 bucket[max_value] = [u]
             else:
                 bucket[max_value].append(u)
-            
+
             # update new location of u
             node_to_num_neighbors[u] = max_value
 
             print("-------- Updated bucket ---------")
             print(bucket)
             print()
-        
-        # nodes = temp_nodes
-        H = H_temp
 
+        nodes = temp_nodes
+        H = H_temp
 
 print("\n\nOutput")
 print(core)
-
-
-
-
-
---
+++ utils.py
@@ -0,0 +1,68 @@
+import sys
+
+sys.path.append("HyperNetX")
+import hypernetx as hnx
+
+
+def strong_subgraph(H, vertex_set):
+    """
+    Returns the strong sub-hypergraph of H induced by vertex_set
+    Parameters
+    ----------
+    H: Hypernetx Hypergraph
+    vertex_set: List/set of vertex label
+
+    Returns
+    -------
+    Hypernetx Hypergraph object
+        The strong sub-hypergraph induced by vertex_set
+    """
+    assert isinstance(H, hnx.Hypergraph)
+    if not isinstance(vertex_set, set):
+        X = set(vertex_set)
+    else:
+        X = vertex_set
+    _tempdict = {}  # dictionary for induced edges
+    for e_id, e_i in H.incidence_dict.items():
+        set_e = set(e_i)
+        if set_e.issubset(X):  # If an edge of the original hypergraph is a subset of the vertex set, add it
+            _tempdict[e_id] = e_i
+    return hnx.Hypergraph(_tempdict)
+
+
+def get_number_of_nbrs(H, u):
+    """
+        Returns the number of neighbours of u in hypergraph H
+        Parameters
+        ----------
+        H: Hypernetx Hypergraph
+        u: a vertex label
+
+        Returns
+        -------
+        Integer
+            The number of neighbours of u in H
+    """
+    nbrs = H.neighbors(u)
+    if nbrs is None:  # u is not in H
+        return 0
+    return len(nbrs)
+
+
+def get_nbrs(H, u):
+    """
+        Returns the neighbours of u in hypergraph H
+        Parameters
+        ----------
+        H: Hypernetx Hypergraph
+        u: a vertex label
+
+        Returns
+        -------
+        List
+            The neighbours of u in H. [] if u is not in H.
+    """
+    nbrs = H.neighbors(u)
+    if nbrs is None:  # u is not in H
+        return []
+    return nbrs
