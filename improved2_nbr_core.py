cb56b4b6b56f222542abaa267229aeb101e79231
naheed <naheedcse@gmail.com>
2021-07-17 01:09:00 +0800

    local upper bound

--- improved2_nbr_core.py
+++ improved2_nbr_core.py
@@ -1,8 +1,11 @@
 import sys
+import matplotlib
+matplotlib.use('TkAgg')
 sys.path.append("HyperNetX")
 import hypernetx as hnx
 import math
-import utils
+from copy import deepcopy
+from hgDecompose import utils
 
 scenes = {
     0: ('FN', 'TH'),
@@ -26,28 +29,28 @@ H = hnx.Hypergraph(scenes)
 nodes = list(H.nodes)
 num_nodes = len(nodes)
 core = {} 
-bucket = {} # mapping of number of neighbors, say n, to nodes with exactly n neighbors 
-
+bucket = {}  # mapping of number of neighbors, say n, to nodes with exactly n neighbors
 
 # auxiliary data, that improves efficiency
 # node_to_neighbors = {} # Not necessary, I guess
 node_to_num_neighbors = {} # inverse bucket
 # removed_nodes = []
 
-lb1 = math.inf
-lb2 = {}
-ub1 = -math.inf
+glb = math.inf
+llb = {}
+gub = -math.inf
+lub = {}
 # flag = {}
 # Initial bucket fill-up
 for node in nodes:
     nbrs = H.neighbors(node)
     # neighbors = list(H.neighbors(node))
     len_neighbors = len(nbrs) # this computation can be repeated
-    lb1 = min(lb1,len_neighbors)
-    ub1 = max(ub1,len_neighbors)
+    glb = min(glb,len_neighbors)
+    gub = max(gub,len_neighbors)
     # LB2 computation
     for u in nbrs:
-        lb2[node] = min(lb2.get(node,len_neighbors),len(H.neighbors(u))-1)
+        llb[node] = min(llb.get(node,len_neighbors),len(H.neighbors(u))-1)
     # node_to_neighbors[node] = neighbors
     node_to_num_neighbors[node] = len_neighbors
     # print(node, neighbors)
@@ -68,36 +71,57 @@ print("\n---------- Initial bucket -------")
 print(bucket)
 print()
 
-for k in range(lb1, ub1):
-    while len(bucket.get(k,[])) != 0:
-        v = bucket[k].pop(0) # get first element in the
-        print("k:", k, "node:", v)
-        core[v] = k
+# Compute Local upper bounds
+copy_bucket = deepcopy(bucket)
+inv_bucket = deepcopy(node_to_num_neighbors)
 
-        temp_nodes = nodes[:] # Make a copy of nodes
-        temp_nodes.remove(v)  # V' <- V \ {v}
-        H_temp = utils.strong_subgraph(H, temp_nodes)
-        for u in utils.get_nbrs(H, v):
-
-            if lb2[u] <= k:
-                len_neighbors_u = utils.get_number_of_nbrs(H_temp, u)
-
-                max_value = max(len_neighbors_u, k)
-                print("max core between", k, 'and', len_neighbors_u, "is ", max_value)
-                print("The location of", u, "is updated from", node_to_num_neighbors[u], "to", max_value)
-                bucket[node_to_num_neighbors[u]].remove(u)
-                bucket[max_value].append(u)
-
-                # update new location of u
-                node_to_num_neighbors[u] = max_value
-
-
-            print("-------- Updated bucket ---------")
-            print(bucket)
-            print()
-        nodes = temp_nodes
-        H = H_temp
-
-
+for k in range(glb, gub):
+    while len(copy_bucket.get(k, []))!=0:
+        v = copy_bucket[k].pop(0)
+        lub[v] = k
+        for u in utils.get_nbrs(H,v):
+            if u not in lub:
+                max_value = max(inv_bucket[u]-1, k)
+                copy_bucket[inv_bucket[u]].remove(u)
+                copy_bucket[max_value].append(u)
+                inv_bucket[u] = max_value
+                # if inv_bucket[u] > k:
+                #     copy_bucket[inv_bucket[u]].remove(u)
+                #     inv_bucket[u] -=1
+                #     copy_bucket[inv_bucket[u]].append(u)
+                # else:
+                #     pass
+# for k in range(lb1, ub1):
+#     while len(bucket.get(k, [])) != 0:
+#         v = bucket[k].pop(0) # get first element in the
+#         print("k:", k, "node:", v)
+#         core[v] = k
+#
+#         temp_nodes = nodes[:] # Make a copy of nodes
+#         temp_nodes.remove(v)  # V' <- V \ {v}
+#         H_temp = utils.strong_subgraph(H, temp_nodes)
+#         for u in utils.get_nbrs(H, v):
+#
+#             if lb2[u] <= k:
+#                 len_neighbors_u = utils.get_number_of_nbrs(H_temp, u)
+#
+#                 max_value = max(len_neighbors_u, k)
+#                 print("max core between", k, 'and', len_neighbors_u, "is ", max_value)
+#                 print("The location of", u, "is updated from", node_to_num_neighbors[u], "to", max_value)
+#                 bucket[node_to_num_neighbors[u]].remove(u)
+#                 bucket[max_value].append(u)
+#
+#                 # update new location of u
+#                 node_to_num_neighbors[u] = max_value
+#
+#             print("-------- Updated bucket ---------")
+#             print(bucket)
+#             print()
+#         nodes = temp_nodes
+#         H = H_temp
+print('local upper bound: ')
+print(sorted(lub.items()))
+print('local lower bound: ')
+print(sorted(llb.items()))
 print("\n\nOutput")
 print(core)
